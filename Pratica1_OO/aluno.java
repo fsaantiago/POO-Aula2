3cd986e7ca3e70a1074be2606a12180d8a866121
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-09-01 22:01:51 -0300

    Ctrl+Enter

---
+++ Pratica1_OO/aluno.java
@@ -0,0 +1,8 @@
+package br.com.aula2.exercicios;
+
+public class Main {
+    public static void main(String [] args) {
+        Aluno aluno1 = new Aluno("Ana","SI",123,3,20,30,0);
+        System.out.println(aluno1.avaliarAluno);
+    }
+}

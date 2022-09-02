8f122a14cbf7c464e112d31a3078782f3058a716
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-09-01 21:33:03 -0300

    Ctrl+Enter

--- Pratica1_OO/NotasAlunos.java
+++ Pratica1_OO/NotasAlunos.java
@@ -1,19 +1,27 @@
 package br.com.aula2.exercicios;
-import java.util.Scanner;
 
 public class NotasAlunos {
     private String nome, curso;
     private int matricula, periodo;
 	private float notaAV1, notaAV2, notaAVE;
 
-    NotasAlunos (String nome, String curso, int matricula, int periodo, float notaAV1, float notaAV2, float notaAVE) {
+    public NotasAlunos (String nome, String curso, int matricula, int periodo, float notaAV1, float notaAV2, float notaAVE) {
         super();
         this.nome = nome;
-        this.curso = nome;
+        this.curso = curso;
         this.matricula = matricula;
         this.periodo = periodo;
         this.notaAV1 = notaAV1;
         this.notaAV2 = notaAV2;
         this.notaAVE = notaAVE;
+    }
+    public void alteraNotaAV1 (float nota) {
+        this.notaAV1 = nota;
+    }
+    public void alteraNotaAV2 (float nota) {
+        this.notaAV2 = nota;
+    }
+    public void alteraNotaAVE (float nota) {
+        this.notaAVE = nota;
     }
 }

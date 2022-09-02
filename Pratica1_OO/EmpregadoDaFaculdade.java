ef5ded66ef9fa31fe8af232775270302653a160d
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-09-01 21:15:06 -0300

    Ctrl+Enter

---
+++ Pratica1_OO/EmpregadoDaFaculdade.java
@@ -0,0 +1,10 @@
+public class EmpregadoDaFaculdade (String nome, double salario, int horasAula) {
+	this.nome = nome;
+	this.salario = salario;
+	this.horasAula = horasAula;
+
+	EmpregadoDaFaculdade emp1;
+	emp1 = new EmpregadoDaFaculdade ("Maria", 5000, 20);
+	System.out.println(emp1.get);
+    
+}
---
+++ Pratica1_OO/NotasAlunos.java
@@ -0,0 +1,10 @@
+package br.com.aula2.exercicios;
+import java.util.Scanner;
+
+public class NotasAlunos {
+    private String nome, curso;
+    private int matricula, periodo;
+	private float notaAV1, notaAV2, notaAVE;
+
+
+}

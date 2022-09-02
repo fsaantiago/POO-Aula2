b4eabbd85be106c71a472eb056558582d894ab1b
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-09-01 21:22:10 -0300

    Ctrl+Enter

--- Pratica1_OO/NotasAlunos.java
+++ Pratica1_OO/NotasAlunos.java
@@ -6,5 +6,14 @@ public class NotasAlunos {
     private int matricula, periodo;
 	private float notaAV1, notaAV2, notaAVE;
 
-
+    NotasAlunos (String nome, String curso, int matricula, int periodo, float notaAV1, float notaAV2, float notaAVE) {
+        super();
+        this.nome = nome;
+        this.curso = nome;
+        this.matricula = matricula;
+        this.periodo = periodo;
+        this.notaAV1 = notaAV1;
+        this.notaAV2 = notaAV2;
+        this.notaAVE = notaAVE;
+    }
 }

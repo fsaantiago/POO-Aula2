4d77b7c14e011b288934a790968f1857e5583517
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-09-04 19:18:04 -0300

    Delete Pratica1_OO/Pratica1_OO directory

--- Pratica1_OO/Pratica1_OO/src/br/com/aula2/exercicios/Concessionaria.java
+++
@@ -1,9 +0,0 @@
-package br.com.aula2.exercicios;
-
-public class Concessionaria {
-	String carro;
-	String cliente;
-	Double transacao;
-	
-	
-}
--- Pratica1_OO/Pratica1_OO/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Pratica1 {
-	requires java.base;
-	requires java.desktop;
-	requires java.management;
-}

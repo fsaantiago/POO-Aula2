134aa800f1351d23307277d2c2206fea4c3e7fa7
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2022-12-01 22:20:51 -0300

    Update MainInterface.java

--- AV2/src/br/com/questao2/MainInterface.java
+++ AV2/src/br/com/questao2/MainInterface.java
@@ -3,13 +3,13 @@ import javax.swing.JOptionPane;
 
 public class MainInterface {
     public static void main(String[] args) {
-        AchadoPerdido achadoPerdido = new AchadoPerdido("O teste", "Testando o teste", "Teste", "aprovado");
-        achadoPerdido.completaDados("Foto imagem de teste", "Belo Horizonte", "12-11-2022");
+        AchadoPerdido achadoPerdido = new AchadoPerdido("teste", "teste sendo testado", "Teste do teste", "Achou");
+        achadoPerdido.completaDados("Foto de teste", "New York", "23-12-2022");
         boolean busca = achadoPerdido.buscarTitulo(achadoPerdido.getTitulo());
         if (busca == true) {
-            JOptionPane.showMessageDialog(null, "Titulo não foi encotrado!");
+            JOptionPane.showMessageDialog(null, "Titulo não foi encotrado");
         } else {
-            JOptionPane.showMessageDialog(null, "Não foi possível encontrar esse titulo, tente novamente");
+            JOptionPane.showMessageDialog(null, "Não foi possível encontrar o titulo buscado, tente novamente");
         }
         JOptionPane.showMessageDialog(null, achadoPerdido.verDetalhe());
     }
